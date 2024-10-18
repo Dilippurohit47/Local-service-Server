bb2709c4f51671c6a77940264a3bf881bcafbc16
Dilippurohit47 <dilippurohit204@gmail.com>
2024-10-18 19:03:52 +0530

    sign-up functionality successfull

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -2,15 +2,42 @@ import { Request, Response } from "express";
 import { registerSchema } from "../validations/authValidations.js";
 import { ZodError } from "zod";
 import { formatError } from "../helper.js";
-
-export const SignIn = (req: Request, res: Response) => {
+import prisma from "../config/database.js";
+import bcrypt from "bcrypt";
+export const SignIn = async (req: Request, res: Response) => {
   try {
     const body = req.body;
     const payload = registerSchema.parse(body);
-    res.json(payload);
+
+    let user = await prisma.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+    });
+
+    if (user) {
+      return res.status(422).json({
+        error: {
+          message: "Email already exist try different eamil or login",
+        },
+      });
+    }
+    payload.password = await bcrypt.hash(payload.password, 10);
+
+    await prisma.user.create({
+      data: payload,
+    });
+
+    return res.status(200).json({
+      message: "User created successfully",
+    });
   } catch (error) {
     if (error instanceof ZodError) {
-      return res.status(422).json(formatError(error));
+      const errors = formatError(error);
+      return res.status(422).json({
+        message: "Invalid data",
+        errors,
+      });
     }
     return res.status(500).json("Internal server error");
   }
